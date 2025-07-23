6c78e5d1fbaea17765b935c6d8241f9f5f5399f5
hassnainali <hassnain.ali.13100@gmail.com>
2025-07-23 14:32:21 +0500

    rm sources

--- AzureCommunicationUI/AzureCommunicationUIDemoApp/Sources/AppDelegate.swift
+++ AzureCommunicationUI/AzureCommunicationUIDemoApp/Sources/AppDelegate.swift
@@ -13,17 +13,17 @@ import OSLog
 import AVFoundation
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate, PKPushRegistryDelegate, UNUserNotificationCenterDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     static var orientationLock: UIInterfaceOrientationMask = .all
 
-    let envConfigSubject = EnvConfigSubject()
+//    let envConfigSubject = EnvConfigSubject()
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
-        AppCenter.start(withAppSecret: envConfigSubject.appCenterSecret, services: [Crashes.self])
+//        AppCenter.start(withAppSecret: envConfigSubject.appCenterSecret, services: [Crashes.self])
         self.setupNotifications(application: application)
         return true
     }
@@ -49,63 +49,63 @@ class AppDelegate: UIResponder, UIApplicationDelegate, PKPushRegistryDelegate, U
         return AppDelegate.orientationLock
     }
 
-    func pushRegistry(_ registry: PKPushRegistry, didUpdate pushCredentials: PKPushCredentials, for type: PKPushType) {
-        guard type == .voIP else {
-            return
-        }
-
-        let tokenString = self.tokenString(from: pushCredentials.token)
-        print("VoIP Token: \(tokenString)")
-
-        if let entryViewController = findEntryViewController() {
-            entryViewController.registerDeviceToken(deviceCode: pushCredentials.token)
-        }
-    }
-
-    func pushRegistry(_ registry: PKPushRegistry,
-                      didReceiveIncomingPushWith payload: PKPushPayload,
-                      for type: PKPushType,
-                      completion: @escaping () -> Void) {
-        print("pushRegistry payload: \(payload.dictionaryPayload)")
-        os_log("pushRegistry payload: \(payload.dictionaryPayload)")
-        if isAppInForeground() {
-            os_log("calling demo app: app is in foreground")
-            if let entryViewController = findEntryViewController() {
-                os_log("calling demo app: onPushNotificationReceived")
-                entryViewController.onPushNotificationReceived(dictionaryPayload: payload.dictionaryPayload)
-            }
-        } else {
-            os_log("calling demo app: app is not in foreground")
-            let pushInfo = PushNotification(data: payload.dictionaryPayload)
-            let providerConfig = CXProviderConfiguration()
-            providerConfig.supportsVideo = true
-            providerConfig.maximumCallGroups = 1
-            providerConfig.maximumCallsPerCallGroup = 1
-            providerConfig.includesCallsInRecents = true
-            providerConfig.supportedHandleTypes = [.phoneNumber, .generic]
-            let callKitOptions = CallKitOptions(providerConfig: providerConfig,
-                                                isCallHoldSupported: true,
-                                                provideRemoteInfo: incomingCallRemoteInfo,
-                                                configureAudioSession: configureAudioSession)
-            CallComposite.reportIncomingCall(pushNotification: pushInfo,
-                                             callKitOptions: callKitOptions) { result in
-                if case .success = result {
-                    DispatchQueue.global().async {
-                        if let entryViewController = self.findEntryViewController() {
-                            os_log("calling demo app: onPushNotificationReceivedBackgroundMode")
-                            entryViewController.onPushNotificationReceivedBackgroundMode(
-                                dictionaryPayload: payload.dictionaryPayload)
-                        }
-                    }
-                } else {
-                    os_log("calling demo app: failed on reportIncomingCall")
-                }
-            }
-        }
-    }
+//    func pushRegistry(_ registry: PKPushRegistry, didUpdate pushCredentials: PKPushCredentials, for type: PKPushType) {
+//        guard type == .voIP else {
+//            return
+//        }
+//
+//        let tokenString = self.tokenString(from: pushCredentials.token)
+//        print("VoIP Token: \(tokenString)")
+//
+//        if let entryViewController = findEntryViewController() {
+//            entryViewController.registerDeviceToken(deviceCode: pushCredentials.token)
+//        }
+//    }
+//
+//    func pushRegistry(_ registry: PKPushRegistry,
+//                      didReceiveIncomingPushWith payload: PKPushPayload,
+//                      for type: PKPushType,
+//                      completion: @escaping () -> Void) {
+//        print("pushRegistry payload: \(payload.dictionaryPayload)")
+//        os_log("pushRegistry payload: \(payload.dictionaryPayload)")
+//        if isAppInForeground() {
+//            os_log("calling demo app: app is in foreground")
+//            if let entryViewController = findEntryViewController() {
+//                os_log("calling demo app: onPushNotificationReceived")
+//                entryViewController.onPushNotificationReceived(dictionaryPayload: payload.dictionaryPayload)
+//            }
+//        } else {
+//            os_log("calling demo app: app is not in foreground")
+//            let pushInfo = PushNotification(data: payload.dictionaryPayload)
+//            let providerConfig = CXProviderConfiguration()
+//            providerConfig.supportsVideo = true
+//            providerConfig.maximumCallGroups = 1
+//            providerConfig.maximumCallsPerCallGroup = 1
+//            providerConfig.includesCallsInRecents = true
+//            providerConfig.supportedHandleTypes = [.phoneNumber, .generic]
+//            let callKitOptions = CallKitOptions(providerConfig: providerConfig,
+//                                                isCallHoldSupported: true,
+//                                                provideRemoteInfo: incomingCallRemoteInfo,
+//                                                configureAudioSession: configureAudioSession)
+//            CallComposite.reportIncomingCall(pushNotification: pushInfo,
+//                                             callKitOptions: callKitOptions) { result in
+//                if case .success = result {
+//                    DispatchQueue.global().async {
+//                        if let entryViewController = self.findEntryViewController() {
+//                            os_log("calling demo app: onPushNotificationReceivedBackgroundMode")
+//                            entryViewController.onPushNotificationReceivedBackgroundMode(
+//                                dictionaryPayload: payload.dictionaryPayload)
+//                        }
+//                    }
+//                } else {
+//                    os_log("calling demo app: failed on reportIncomingCall")
+//                }
+//            }
+//        }
+//    }
 
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        self.voipRegistration()
+//        self.voipRegistration()
     }
 
     public func configureAudioSession() -> Error? {
@@ -119,16 +119,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate, PKPushRegistryDelegate, U
         return configError
     }
 
-    public func incomingCallRemoteInfo(info: Caller) -> CallKitRemoteInfo {
-        let cxHandle = CXHandle(type: .generic, value: "Incoming call")
-        var remoteInfoDisplayName = envConfigSubject.callkitRemoteInfo
-        if remoteInfoDisplayName.isEmpty {
-            remoteInfoDisplayName = info.displayName
-        }
-        let callKitRemoteInfo = CallKitRemoteInfo(displayName: remoteInfoDisplayName,
-                                                               handle: cxHandle)
-        return callKitRemoteInfo
-    }
+//    public func incomingCallRemoteInfo(info: Caller) -> CallKitRemoteInfo {
+//        let cxHandle = CXHandle(type: .generic, value: "Incoming call")
+//        var remoteInfoDisplayName = envConfigSubject.callkitRemoteInfo
+//        if remoteInfoDisplayName.isEmpty {
+//            remoteInfoDisplayName = info.displayName
+//        }
+//        let callKitRemoteInfo = CallKitRemoteInfo(displayName: remoteInfoDisplayName,
+//                                                               handle: cxHandle)
+//        return callKitRemoteInfo
+//    }
 
     private func isAppInForeground() -> Bool {
         let appState = UIApplication.shared.applicationState
@@ -142,30 +142,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate, PKPushRegistryDelegate, U
     }
 
     // Register for VoIP notifications
-    private func voipRegistration() {
-        let mainQueue = DispatchQueue.main
-
-        // Create a push registry object
-        let voipRegistry = PKPushRegistry(queue: mainQueue)
-        // Set the registry's delegate to self
-        voipRegistry.delegate = self
-        // Set the push type to VoIP
-        voipRegistry.desiredPushTypes = [PKPushType.voIP]
-    }
-
-    private func tokenString(from data: Data) -> String {
-        return data.map { String(format: "%02.2hhx", $0) }.joined()
-    }
-
-    private func findEntryViewController() -> EntryViewController? {
-        guard let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-              let window = scene.windows.first,
-              let rootViewController = window.rootViewController as? UINavigationController,
-              let entryViewController = rootViewController.viewControllers.first as? EntryViewController else {
-            return nil
-        }
-        return entryViewController
-    }
+//    private func voipRegistration() {
+//        let mainQueue = DispatchQueue.main
+//
+//        // Create a push registry object
+//        let voipRegistry = PKPushRegistry(queue: mainQueue)
+//        // Set the registry's delegate to self
+//        voipRegistry.delegate = self
+//        // Set the push type to VoIP
+//        voipRegistry.desiredPushTypes = [PKPushType.voIP]
+//    }
+//
+//    private func tokenString(from data: Data) -> String {
+//        return data.map { String(format: "%02.2hhx", $0) }.joined()
+//    }
+//
+//    private func findEntryViewController() -> EntryViewController? {
+//        guard let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+//              let window = scene.windows.first,
+//              let rootViewController = window.rootViewController as? UINavigationController,
+//              let entryViewController = rootViewController.viewControllers.first as? EntryViewController else {
+//            return nil
+//        }
+//        return entryViewController
+//    }
 
     private func setupNotifications(application: UIApplication) {
         UNUserNotificationCenter.current().delegate = self
--- AzureCommunicationUI/AzureCommunicationUIDemoApp/Sources/SceneDelegate.swift
+++ AzureCommunicationUI/AzureCommunicationUIDemoApp/Sources/SceneDelegate.swift
@@ -11,13 +11,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     fileprivate func assignCallID(_ callID: String, _ appDelegate: AppDelegate) {
-        if callID.contains("http") {
-            appDelegate.envConfigSubject.teamsMeetingLink = callID
-            appDelegate.envConfigSubject.selectedMeetingType = .teamsMeeting
-        } else {
-            appDelegate.envConfigSubject.groupCallId = callID
-            appDelegate.envConfigSubject.selectedMeetingType = .groupCall
-        }
+//        if callID.contains("http") {
+//            appDelegate.envConfigSubject.teamsMeetingLink = callID
+//            appDelegate.envConfigSubject.selectedMeetingType = .teamsMeeting
+//        } else {
+//            appDelegate.envConfigSubject.groupCallId = callID
+//            appDelegate.envConfigSubject.selectedMeetingType = .groupCall
+//        }
     }
 
     func scene(_ scene: UIScene,
@@ -27,44 +27,44 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
               let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
             return
         }
-        let entryViewController = EntryViewController(envConfigSubject: appDelegate.envConfigSubject)
-        let rootNavController = UINavigationController(rootViewController: entryViewController)
-        let userActivity = connectionOptions.userActivities.first
-        if let intent = userActivity?.interaction?.intent as? INStartCallIntent,
-           let callID = intent.contacts?.first?.personHandle?.value {
-            assignCallID(callID, appDelegate)
-        }
-        let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = rootNavController
-        self.window = window
-        window.makeKeyAndVisible()
-
-        // Handle deep link jump from re-launch
-        let urlContexts = connectionOptions.urlContexts
-        if let queryDict = urlContexts.first?.url.toQueryDictionary() {
-            appDelegate.envConfigSubject.update(from: queryDict)
-        } else {
-            appDelegate.envConfigSubject.load()
-        }
+//        let entryViewController = EntryViewController(envConfigSubject: appDelegate.envConfigSubject)
+//        let rootNavController = UINavigationController(rootViewController: entryViewController)
+//        let userActivity = connectionOptions.userActivities.first
+//        if let intent = userActivity?.interaction?.intent as? INStartCallIntent,
+//           let callID = intent.contacts?.first?.personHandle?.value {
+//            assignCallID(callID, appDelegate)
+//        }
+//        let window = UIWindow(windowScene: windowScene)
+//        window.rootViewController = rootNavController
+//        self.window = window
+//        window.makeKeyAndVisible()
+//
+//        // Handle deep link jump from re-launch
+//        let urlContexts = connectionOptions.urlContexts
+//        if let queryDict = urlContexts.first?.url.toQueryDictionary() {
+//            appDelegate.envConfigSubject.update(from: queryDict)
+//        } else {
+//            appDelegate.envConfigSubject.load()
+//        }
     }
 
     func scene(_ scene: UIScene,
                openURLContexts URLContexts: Set<UIOpenURLContext>) {
-        if let queryDict = URLContexts.first?.url.toQueryDictionary(),
-           let appDelegate = UIApplication.shared.delegate as? AppDelegate {
-            appDelegate.envConfigSubject.update(from: queryDict)
-        } else {
-            if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
-                appDelegate.envConfigSubject.load()
-            }
-        }
+//        if let queryDict = URLContexts.first?.url.toQueryDictionary(),
+//           let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+//            appDelegate.envConfigSubject.update(from: queryDict)
+//        } else {
+//            if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+//                appDelegate.envConfigSubject.load()
+//            }
+//        }
     }
 
     func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
-        if let intent = userActivity.interaction?.intent as? INStartCallIntent,
-           let callID = intent.contacts?.first?.personHandle?.value,
-           let appDelegate = UIApplication.shared.delegate as? AppDelegate {
-            assignCallID(callID, appDelegate)
-        }
+//        if let intent = userActivity.interaction?.intent as? INStartCallIntent,
+//           let callID = intent.contacts?.first?.personHandle?.value,
+//           let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+//            assignCallID(callID, appDelegate)
+//        }
     }
 }
